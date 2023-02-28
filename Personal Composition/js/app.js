f5125a1309142189a1a440b7ef2cbed45b177a46
Sunny Mason <122555842+sunnydaygamer@users.noreply.github.com>
2023-02-27 20:19:28 -0500

    Add files via upload

---
+++ Personal Composition/js/app.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(800,600);
+}
+
+function draw() {
+    fill(237, 194, 119);
+    ellipse(155, 155, 300, 300);
+
+    fill(0, 0, 0);
+    ellipse(75, 100, 50, 50);
+    ellipse(225, 100, 50, 50);
+
+    fill(125, 11, 30);
+    ellipse(155, 225, 75, 75);
+    
+    fill(185, 45, 69);
+    ellipse(155, 250, 45, 25);
+
+}
