0af86b734816b43ecb7da78c0d11d1c7a08fe8e6
Sunny Mason <122555842+sunnydaygamer@users.noreply.github.com>
2023-04-27 23:44:39 -0400

    Add files via upload

--- final project/js/app.js
+++ final project/js/app.js
@@ -7,43 +7,42 @@ var cardTotal = 16;
 //event listener for cards
 cards.forEach(card => {
   card.addEventListener('click', () => {
-    colorChange(card);
+    changeColor(card);
   });
 });
 
 //changes colors of cards cliced, then updates variables for count, checks if player flipped two cards
-function colorChange(card) {
-  card.style.backgroundColor = card.getAttribute("cardColor");
+function changeColor(card) {
+  card.style.backgroundColor = card.getAttribute("data-color");
   cardNumber++;
   cardsFlipped.push(card);
   console.log(cardsFlipped.length);
   if (cardNumber === 2) { 
-    const cards1 = cardsFlipped[0];
-    const cards2 = cardsFlipped[1];
+    const card1 = cardsFlipped[0];
+    const card2 = cardsFlipped[1];
     
     //checks if the cards match, then will remove them after 1 second if matches
     //the "else" will reset cards also after 1 second
     //then it reloads the page/resets the cards
-    if (cards1.getAttribute("cardColor") === cards2.getAttribute("cardColor")) {
-        cardTotal = cardTotal - 2;
-        cardTotal.log(cardTotal);
-      //This will remove the cards after two seconds
+    if (card1.getAttribute("data-color") === card2.getAttribute("data-color")) {
+      cardTotal = cardTotal - 2;
+      console.log(cardTotal);
       setTimeout(remove, 1000);
       function remove() {
-        cards1.style.backgroundColor = "white";
-        cards2.style.backgroundColor = "white";
+        card1.style.backgroundColor = "white";
+        card2.style.backgroundColor = "white";
       }
     } else {
       setTimeout(reset, 1000);
       function reset() {
-        cards1style.backgroundColor = "black";
-        cards2.style.backgroundColor = "black";
+        card1.style.backgroundColor = "black";
+        card2.style.backgroundColor = "black";
       }
     } if (cardTotal == 0){
         location.reload(true)
-        console.log("reloaded cards")
+        console.log("it worked")
     }
-    cardsFlipped = [];
+    cardsFlipped= [];
     cardNumber = 0;
 }
 }    
